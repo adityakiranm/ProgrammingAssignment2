82f46cc9203df74846222c06e28414ba279f0981
adityakiranm <adityakiran.m@gmail.com>
2017-03-12 10:37:43 +0530

    Added descriptive comments to CacheSolve.R function
    Added comments to explain the function CacheSolve.R

--- CacheSolve.R
+++ CacheSolve.R
@@ -1,3 +1,7 @@
+# CacheSolve function will return the inverse of a matrix. If the Inverse for the
+# matrix is already available in Cache, the function will return the same and NOT
+# re-compute it again
+
 cacheSolve <- function(x, ...) {
   m <- x$getInverse()
   if(!is.null(m)) {
